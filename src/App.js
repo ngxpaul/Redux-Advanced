fdc6b3dce389214405733ed8e1d22bbd40345aae
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-22 16:53:42 +0700

    fetch data with firebase using PUT method to send data to the database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    //remember that create firebase database in "test mode" to allow read and write
+    fetch('https://react-cdec5-default-rtdb.firebaseio.com/cart.json', {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+    //PUT >< POST -> PUT : new data will not be added in a list of data -> it will override the existing data
+
+  }, [cart]);
 
   return (
     <Layout>
