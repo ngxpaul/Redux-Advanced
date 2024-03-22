9b9ace4cefed666d65d84b3bea8515a4cbe3691f
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-22 17:11:21 +0700

    dispatch show Notification sucess & error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { uiActions } from "./ui-slice";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -9,13 +10,34 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   useEffect(() => {
-    //remember that create firebase database in "test mode" to allow read and write
-    fetch('https://react-cdec5-default-rtdb.firebaseio.com/cart.json', {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
-    //PUT >< POST -> PUT : new data will not be added in a list of data -> it will override the existing data
-
+    const sendCartData = async () => {
+      //remember that create firebase database in "test mode" to allow read and write
+      const response = await fetch(
+        "https://react-cdec5-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if(!response.ok){
+        dispatch(
+          uiActions.showNotification({
+            status: "Error",
+            title: "Error...",
+            message: "Sending cart data failed!",
+          })
+        );
+      }
+      //PUT >< POST -> PUT : new data will not be added in a list of data -> it will override the existing data
+      const responseData = await response.json();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sending cart data successfully!",
+        })
+      );
+    };
   }, [cart]);
 
   return (
