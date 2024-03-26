5d268f79719d45f26cfc11c544d03b1a7d555fbe
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-26 13:35:41 +0700

    connect sucessful with firebase and update quantity directly to database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useSelector } from "react-redux";
-import { uiActions } from "./ui-slice";
-
+import { uiActions } from "./store/ui-slice";
+import { useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
+import Notification from "./components/UI/Notification";
+ let initialStart = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
     const sendCartData = async () => {
       //remember that create firebase database in "test mode" to allow read and write
@@ -19,14 +23,8 @@ function App() {
           body: JSON.stringify(cart),
         }
       );
-      if(!response.ok){
-        dispatch(
-          uiActions.showNotification({
-            status: "Error",
-            title: "Error...",
-            message: "Sending cart data failed!",
-          })
-        );
+      if (!response.ok) {
+        throw new Error("Seding cart data failed");
       }
       //PUT >< POST -> PUT : new data will not be added in a list of data -> it will override the existing data
       const responseData = await response.json();
@@ -38,13 +36,36 @@ function App() {
         })
       );
     };
-  }, [cart]);
+    if (initialStart) {
+      initialStart = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Error",
+          title: "Error...",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
